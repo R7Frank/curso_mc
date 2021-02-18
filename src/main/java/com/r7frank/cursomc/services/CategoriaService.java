71e7835de08623d02ca02b94336a892b407e6737
R7Frank <r7frank@hotmail.com>
2021-02-18 18:05:58 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/r7frank/cursomc/services/CategoriaService.java
+++ src/main/java/com/r7frank/cursomc/services/CategoriaService.java
@@ -2,11 +2,13 @@ package com.r7frank.cursomc.services;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.r7frank.cursomc.domain.Categoria;
 import com.r7frank.cursomc.repositories.CategoriaRepository;
+import com.r7frank.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,9 +16,10 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
 
 }
