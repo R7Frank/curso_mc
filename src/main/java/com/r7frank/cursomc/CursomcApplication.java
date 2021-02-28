58da8ba2cd5b0cad106c9c4387a512d2bc4fec8b
R7Frank <r7frank@hotmail.com>
2021-02-28 04:16:50 -0300

    Criacao de Pedido, Pagamento e EstadoPagamento

--- src/main/java/com/r7frank/cursomc/CursomcApplication.java
+++ src/main/java/com/r7frank/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.r7frank.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.r7frank.cursomc.domain.Cidade;
 import com.r7frank.cursomc.domain.Cliente;
 import com.r7frank.cursomc.domain.Endereco;
 import com.r7frank.cursomc.domain.Estado;
+import com.r7frank.cursomc.domain.Pagamento;
+import com.r7frank.cursomc.domain.PagamentoComBoleto;
+import com.r7frank.cursomc.domain.PagamentoComCartao;
+import com.r7frank.cursomc.domain.Pedido;
 import com.r7frank.cursomc.domain.Produto;
+import com.r7frank.cursomc.domain.enums.EstadoPagamento;
 import com.r7frank.cursomc.domain.enums.TipoCliente;
 import com.r7frank.cursomc.repositories.CategoriaRepository;
 import com.r7frank.cursomc.repositories.CidadeRepository;
 import com.r7frank.cursomc.repositories.ClienteRepository;
 import com.r7frank.cursomc.repositories.EnderecoRepository;
 import com.r7frank.cursomc.repositories.EstadoRepository;
+import com.r7frank.cursomc.repositories.PagamentoRepository;
+import com.r7frank.cursomc.repositories.PedidoRepository;
 import com.r7frank.cursomc.repositories.ProdutoRepository;
 
 
@@ -42,6 +50,12 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -91,8 +105,21 @@ public class CursomcApplication implements CommandLineRunner{
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/01/2017 19:35"), cli1, e2);
 		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("10/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 
 }
--- src/main/java/com/r7frank/cursomc/domain/Cliente.java
+++ src/main/java/com/r7frank/cursomc/domain/Cliente.java
@@ -14,7 +14,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.r7frank.cursomc.domain.enums.TipoCliente;
 
@@ -31,16 +30,17 @@ public class Cliente implements Serializable {
 	private Integer tipo;
 	
 	@JsonManagedReference
-	@OneToMany(mappedBy = "cliente")
+	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String>	telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
-	
 	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
@@ -108,6 +108,14 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -132,7 +140,5 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 
 }
