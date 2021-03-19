0a53d710275918fddca5ab660a71024a1cb9b6d1
R7Frank <r7frank@hotmail.com>
2021-03-18 23:37:30 -0300

    Disponibilizacao do endpoint /pedidos/{id}

--- src/main/java/com/r7frank/cursomc/domain/ItemPedido.java
+++ src/main/java/com/r7frank/cursomc/domain/ItemPedido.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
@@ -30,6 +33,7 @@ public class ItemPedido implements Serializable {
 
 	// Observação: até o momento só foram criados os "gets" de Pedido e Produto, não foram criados os "sets" para eles.
 	
+	@JsonIgnore
 	public Pedido getPedido( ) {
 		return id.getPedido();
 	}
